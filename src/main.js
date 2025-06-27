09f0e0b0ec939294384a9b835e527c2f406f057f
inno-site <innouiux2@gmail.com>
2025-06-27 11:43:30 +0900

    build

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/tailwind.css'
+// import Components from './components/Global/'
+import './assets/css/tailwind.css'
+import './assets/css/font.css'
+import './assets/css/common.css'
 
 const app = createApp(App)
 app.use(router)
+// app.use(Components);
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+		<div class="text-3xl text-blue-500">Tailwind 작동 🎉</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
